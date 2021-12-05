1b75eea72c7d9491d1a07f7619268cd5f1bd9ed3
Beni <benimadhab11@gmail.com>
2021-12-05 09:53:12 +0530

    Project Setup Files

---
+++ src/main/java/com/up42/codingtask/UP42Main.java
@@ -0,0 +1,15 @@
+package com.up42.codingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UP42Main
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(UP42Main.class, args);
+    }
+
+}
---
+++ src/test/java/com/up42/codingtask/UP42MainTests.java
@@ -0,0 +1,14 @@
+//package com.up42.codingtask;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UP42MainTests
+//{
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
